ba9144a4990b931c406ef796d69624d6439dcf87
Carolina Duarte <carolcrduarte@knights.ucf.edu>
2022-09-03 16:00:38 -0400

    non static play method written

--- Game.java
+++ Game.java
@@ -4,35 +4,63 @@
    Programming Assignment 1
 */
 
-import java.util.Arrays;
-import java.util.Scanner;
+//import java.io.File;
 
 public class Game {
-   public static void main(String [] args) throws Exception
-	{
-      int i = 8;
-      int j = 8;
-      int[][] board = new int[i][j]; //Initializes an 8x8 board
+
+   //Constructor with two parameters:
+   Game(int board_size, String obj) 
+   {       
+      //int[][] board = new int[board_size][board_size]; 
   
-      //This creates the 8x8 board and fills the board with asterisks
+      /*This creates the 8x8 board and fills the board with zeros.
       for (int row = 0; row < board.length; row++) {
           for (int column = 0; column < board[0].length; column++) {
               board[row][column] = 0;
           }
      
-          for (int column=0; column<8; column++) { //creates right amount of cells for the 8 columns
-              System.out.print(board[row][column] + " "); //prints # of cells
+          for (int column=0; column<8; column++) { //Creates right amount of cells for the 8 columns.
+              System.out.print(board[row][column] + " "); //Prints # of cells.
           }
-                  System.out.println(); //prints each row on new line
+                  System.out.println(); //Prints each row on new line.
+      }
+      readMoves(obj);
+      */
+   }
+
+   //Scans the respective text files of moves and store them in the character array attribute:
+   /*public void readMoves (String obj) throws FileNotFoundException 
+   {
+      String theString = "";
+
+      File file = new File(obj);
+      try (Scanner scanner = new Scanner(file)) {
+         theString = scanner.nextLine();
+         while (scanner.hasNextLine()) {
+         theString = theString + "\n" + scanner.nextLine();
+         }
       }
 
-      char[] moves_array = {'r', 'b', 'd'};
-       
-}
-}
+      char[] charArray = theString.toCharArray();
+      System.out.println(charArray);
+   }
+   */
+ 
+   public int play (int winner)
+   {
+      if (winner == 1) { //Player 1 winning strategy.  
+         //The text files provided is only for player 2's moves.
+         //This means you will use these moves when coming up with player 1's strategy to win.
+                 
+         return 1;
+      }
+      else { //Player 2 winning strategy.
+         //The text files is not used when coming up with player 2's strategy to win.
+         //As for player 2's strategy, you will have to think carefully what must happen in order for player 2 to win.
 
+         return 2;   
+      }
+   }
 
+} //End of code.
 
-
-
-    
